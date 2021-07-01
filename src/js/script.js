c9ecb7df80aff4539ef7c03279560f1406e0a21a
Ali-Hassan-ALi <ah965961@gmail.com>
2021-07-01 15:09:25 +0300

    change new vwetion and add vetcher

--- src/js/script.js
+++ src/js/script.js
@@ -6,14 +6,17 @@ $("document").ready(function() {
         $(this).addClass("active");
     });
 
-
+    //on click nice scrollTop
     $("a").click(function() {
-            var e = $(this).attr("href");
-            $("html, body").animate({ scrollTop: $(e).offset().top }, 1e3);
-        }),
+        var e = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(e).offset().top }, 1e3);
+    });
 
-        //nice Scroll
-        $("html").niceScroll({ interval: 9e3 }); /*end of nice Scroll*/
+    //nice Scroll
+    $("html").niceScroll({ interval: 9e3 }); /*end of nice Scroll*/
+
+    //wow js
+    new WOW().init();
 
     $('#OwlCarouse .owl-category').owlCarousel({
         loop: false,
@@ -41,8 +44,16 @@ $("document").ready(function() {
                 items: 6
             }
         }
+    }); //end of owl Carouse
+
+    //searching function
+    $(document).ready(function() {
+        $("#searching").hover(function() {
+            $('.search-btn').toggleClass('btn btn-danger');
+        });
     });
 
+    //slider height full screen
     // $('.slider').height($(window).height());
 
     // $(window).resize(function() {
@@ -51,4 +62,40 @@ $("document").ready(function() {
 
     // });
 
+    //change profile images
+    $("#profileImage").click(function(e) {
+        $("#imageUpload").click();
+    });
+
+    function fasterPreview(uploader) {
+        if (uploader.files && uploader.files[0]) {
+            $('#profileImage').attr('src',
+                window.URL.createObjectURL(uploader.files[0]));
+        }
+    }
+
+    $("#imageUpload").change(function() {
+        fasterPreview(this);
+    });
+
+
+
 }); /*end of document ready*/
+
+
+
+// $('body').append('<div id="loadingDiv" class="lds-ripple"><div></div><div></div></div>');
+
+// $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+// $(window).on('load', function() {
+// setTimeout(removeLoader, 100000); //wait for page load PLUS two seconds.
+// });
+
+// function removeLoader() {
+// $("#loadingDiv").fadeOut(10000, function() {
+// fadeOut complete. Remove the loading div
+// $('.lds-ripple').css('display', 'block');
+// $('.lds-ripple').css('pozation', 'absloute');
+// $("#loadingDiv").remove(); //makes page more lightweight 
+// });
+// }
